672bc893afd8a0566de6184c81eb05ee40ac6209
Salt Developer <matilda.lindahl@gmail.com>
2022-07-18 21:13:34 +0200

    changed host env to make it work in deployment

--- database/connectionPool.js
+++ database/connectionPool.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');// const db = require('./db');
 
 const pool = new Pool({  
   user: process.env.DB_USER, 
-  host: process.env.HOST, 
+  host: process.env.DB_HOST, 
   database: process.env.DATABASE, 
   password: process.env.PASSWORD, 
   port: process.env.DB_PORT,
