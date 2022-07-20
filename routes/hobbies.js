660b5f8ec708b17602c35c55ba717b9c7a5b9ed3
Salt Developer <matilda.lindahl@gmail.com>
2022-07-20 17:24:10 +0200

    added routes for hobbies

--- routes/hobbies.js
+++ routes/hobbies.js
@@ -4,7 +4,6 @@ const { pool } = require('../database/connectionPool');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  console.log(req.session);
 
   // const { rows } = await pool.query('SELECT username FROM "Users" WHERE username = $1', [name]);
 
@@ -14,11 +13,28 @@ router.get('/', async (req, res) => {
       res.status(400).json({ error: 'DB has no hobbies' });
       return;
     }
-    console.log(rows);
+    // console.log(rows);
     res.json({ hobbies: rows });
   } catch (caughtError) {
     res.status(500).json({ error: caughtError.message });
   }
 });
+
+router.get('/:hobbyname', async (req, res) => {
+  console.log('hello');
+  const hobbyName = req.params.hobbyname;
+  console.log(hobbyName);
+  try {
+    const { rows } = await pool.query('SELECT * FROM "Hobbies" WHERE hobbyname ILIKE  $1', [hobbyName]);
+    if (rows.length === 0) {
+      res.status(400).json({ error: 'No such hobby in our DB' });
+      return;
+    }
+    console.log(rows);
+    res.json({ hobby: rows[0] });
+  } catch (caughtError) {
+    res.status(500).json({ error: caughtError.message });
+  }
+})
 
 module.exports = router;
