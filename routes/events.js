868e5ca78fd4384e557b846b319ee914f4172850
Salt Developer <saltdeverloper@DEV160.local>
2022-07-22 14:45:09 +0200

    C of Crud completed

--- routes/events.js
+++ routes/events.js
@@ -18,16 +18,14 @@ router.get('/', async (req, res) => {
 })
 
 router.get('/:hobbyname', async (req, res) => {
-    console.log('hello');
     const hobbyName = req.params.hobbyname;
-    console.log(hobbyName);
      try {
       const { rows } = await pool.query('SELECT * FROM "Events" WHERE hobbyname ILIKE $1', [hobbyName]);
       if (rows.length === 0) {
         res.status(400).json({ error: 'No events in our DB' });
         return;
       }
-      console.log(rows);
+      // console.log(rows);
       res.json({ events: rows });
     } catch (caughtError) {
       res.status(500).json({ error: caughtError.message });
@@ -37,17 +35,13 @@ router.get('/:hobbyname', async (req, res) => {
 router.post('/:hobbyname', async (req, res) => {
     const hobbyName = req.params.hobbyname;
     const inputObject = req.body;
-    console.log(inputObject);
-  
-    // try {
-    //   await pool.query('INSERT INTO "Users" (username, password) VALUES ($1, $2)', [name, password]);
-    // } catch (caughtError) {
-    //   res.status(500).json({ error: caughtError.message });
-    //   return;
-    // }
-  
-    // res.json({ message: 'You might have added the event' });
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    try {
+      await pool.query('INSERT INTO "Events" (hobbyname, eventname, eventdescription, eventlocation, eventtime) VALUES ($1, $2, $3, $4, $5)', [hobbyName, inputObject.eventName, inputObject.eventDescription, inputObject.eventLocation, inputObject.eventTime]);
+    } catch (caughtError) {
+      return res.status(500).json({ error: caughtError.message }); 
+    }
+    res.json({ message: 'You have added the event' });
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   });
 
 module.exports = router;
