af525de75c741a1a004825bb4b6ee53102e38517
Salt Developer <matilda.lindahl@gmail.com>
2022-07-25 16:29:39 +0200

    added crud

--- routes/events.js
+++ routes/events.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { pool } = require('../database/connectionPool');
+const asyncHandler = require('express-async-handler')
 
 const router = express.Router();
 
@@ -26,7 +27,6 @@ router.get('/:hobbyname', async (req, res) => {
         res.status(400).json({ error: 'No events in our DB' });
         return;
       }
-      // console.log(rows);
       res.json({ events: rows });
     } catch (caughtError) {
       res.status(500).json({ error: caughtError.message });
@@ -36,13 +36,29 @@ router.get('/:hobbyname', async (req, res) => {
 router.post('/:hobbyname', async (req, res) => {
     const hobbyName = req.params.hobbyname;
     const inputObject = req.body;
+    console.log(req.body);
     try {
-      await pool.query('INSERT INTO "Events" (hobbyname, eventname, eventdescription, eventlocation, eventtime) VALUES ($1, $2, $3, $4, $5)', [hobbyName, inputObject.eventName, inputObject.eventDescription, inputObject.eventLocation, inputObject.eventTime]);
+      await pool.query('INSERT INTO "Events" (hobbyname, eventname, eventdescription, eventlocation, eventtime, creator) VALUES ($1, $2, $3, $4, $5, $6)', [hobbyName, inputObject.eventName, inputObject.eventDescription, inputObject.eventLocation, inputObject.eventTime, req.session.user.user]);
+      // await pool.query('INSERT INTO "Events" (hobbyname, eventname, eventdescription, eventlocation, eventtime) VALUES ($1, $2, $3, $4, $5)', [hobbyName, inputObject.eventName, inputObject.eventDescription, inputObject.eventLocation, inputObject.eventTime]);
     } catch (caughtError) {
       return res.status(500).json({ error: caughtError.message }); 
     }
     res.json({ message: 'You have added the event' });
     // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   });
+
+router.put('/:hobbyname/:id', asyncHandler(async (req, res) => {
+  const inputObject = req.body;
+  if(inputObject.creator !== req.session.user.user) return res.status(401).json({ error: 'You can only edit your own events.'})
+  // await pool.query('DELETE FROM "Events" WHERE id = $1', [+req.params.id]);
+  await pool.query('UPDATE "Events" SET eventname = $1, eventdescription = $2, eventlocation = $3, eventtime = $4 WHERE id = $5', [inputObject.eventName, inputObject.eventDescription, inputObject.eventLocation, inputObject.eventTime, +req.params.id]);
+  res.json({message: 'You might have updated the event'});
+}));
+
+router.delete('/:hobbyname/:id', asyncHandler(async (req, res) => {
+  if(req.body.creator !== req.session.user.user) return res.status(401).json({ error: 'You can only delete your own events.'})
+  await pool.query('DELETE FROM "Events" WHERE id = $1', [+req.params.id]);
+  res.json({message: 'Successfully deleted'});
+}));
 
 module.exports = router;
